f5be28991edf1386c2b7e64cc87e9b84a1580964
tron0318 <tron0318@naver.com>
2024-07-22 18:24:54 +0900

    question,answer default

---
+++ src/main/java/com/example/ArtAuction_24/answer/controller/AnswerController.java
@@ -0,0 +1,10 @@
+package com.example.ArtAuction_24.answer.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class AnswerController {
+}
