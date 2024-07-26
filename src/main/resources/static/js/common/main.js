2870fcf42c37b4d35dd88ba9d3e53ec9546d3687
kimminseop99 <minseopkim99@gmail.com>
2024-07-26 15:50:11 +0900

    rebase

--- src/main/resources/static/js/common/main.js
+++ src/main/resources/static/js/common/main.js
@@ -69,3 +69,21 @@ var swiper1 = new Swiper(".mySwiper", {
 
 
 /*upcoming js end*/
+
+/*guide js start*/
+var $owl = $('.loop');
+
+$owl.owlCarousel({
+    autoplay: true,
+    autoplayHoverPause: true,
+    autoplayTimeout: 3000,
+    autoplaySpeed: 800,
+    center: true,
+    items: 1.4,
+    stagePadding: 15,
+    loop: true,
+    margin: 15,
+  animateOut: 'slide-up',
+animateIn: 'slide-down',
+});
+/*guide js end*/
