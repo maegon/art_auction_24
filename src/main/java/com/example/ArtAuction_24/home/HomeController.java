3a7114bc6070592d8e51bd517a018b78cf5ae606
kimminseop99 <161572638+kimminseop99@users.noreply.github.com>
2024-07-26 16:11:43 +0900

    Enhancement/11 (#18)
    * swbestArtist 생성
    
    * rebase
    
    * rebase

--- src/main/java/com/example/ArtAuction_24/home/HomeController.java
+++ src/main/java/com/example/ArtAuction_24/home/HomeController.java
@@ -3,6 +3,7 @@ package com.example.ArtAuction_24.home;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
@@ -10,5 +11,10 @@ public class HomeController {
     @GetMapping("/")
     public String index() {
         return "home/main";
+    }
+
+    @GetMapping("/home/introduce")
+    public String introduce() {
+        return "home/introduce";
     }
 }
--- src/main/resources/static/js/common/main.js
+++ src/main/resources/static/js/common/main.js
@@ -12,7 +12,7 @@ var swiper1 = new Swiper(".mySwiper", {
     slidesPerView: 1,
     spaceBetween: 10,
     pagination: {
-      el: ".swiper-pagination",
+      el: ".ongoing-pagination",
       clickable: true,
     },
     // 반응형 breakpoints : px 기준
@@ -37,3 +37,53 @@ var swiper1 = new Swiper(".mySwiper", {
 
 
 /*ongoing js end*/
+
+/*upcoming js start*/
+ var swiper3 = new Swiper(".upcomingAuc", {
+
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+      el: ".upcoming-pagination",
+      clickable: true,
+    },
+    // 반응형 breakpoints : px 기준
+    breakpoints: {
+      // window 넓이 640px ~ 767px
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 40,
+      },
+      // window 넓이 768px ~ 1023px
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 60,
+      },
+      // window 넓이 1024px ~
+      1024: {
+        slidesPerView: 4,
+        spaceBetween: 80,
+      },
+    },
+    });
+
+
+/*upcoming js end*/
+
+/*guide js start*/
+var $owl = $('.loop');
+
+$owl.owlCarousel({
+    autoplay: true,
+    autoplayHoverPause: true,
+    autoplayTimeout: 3000,
+    autoplaySpeed: 800,
+    center: true,
+    items: 1.4,
+    stagePadding: 15,
+    loop: true,
+    margin: 15,
+  animateOut: 'slide-up',
+animateIn: 'slide-down',
+});
+/*guide js end*/
