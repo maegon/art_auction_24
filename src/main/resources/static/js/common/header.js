45f9f8689a746926b0aa7df7745faba4bb73858e
kimminseop99 <minseopkim99@gmail.com>
2024-07-28 19:54:55 +0900

    rebase

--- src/main/resources/static/js/common/header.js
+++ src/main/resources/static/js/common/header.js
@@ -1,10 +1,24 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const toggleBtn = document.querySelector('.nav__bar');
-    const navMain = document.querySelector('.nav__main');
-    const navLogin = document.querySelector('.nav__login');
+function toggleMenu() {
+    document.querySelector('.nav__main').classList.toggle('active');
+    document.querySelector('.nav__login').classList.toggle('active');
+    document.querySelector('.header_nav').classList.toggle('active');
+}
 
-    toggleBtn.addEventListener('click', () => {
-        navMain.classList.toggle('active');
-        navLogin.classList.toggle('active');
+
+
+/*$(document).ready(function() {
+    $('#search_button').click(function() {
+        var $searchInput = $('#search_input');
+        var $searchContainer = $('#search_container');
+
+        $searchInput.toggleClass('show');
+        $searchInput.focus(); // 검색창이 보일 때 포커스 설정
+
+        // `show` 클래스에 따라 `border-bottom`을 추가하거나 제거
+        if ($searchInput.hasClass('show')) {
+            $searchContainer.css('border-bottom', '1px solid black');
+        } else {
+            $searchContainer.css('border-bottom', 'none');
+        }
     });
-});
+});*/
