9f7b5606f3fc59c0c1a83536ec3e7fec642fd611
kimminseop99 <161572638+kimminseop99@users.noreply.github.com>
2024-07-31 18:04:09 +0900

    Enhancement/37 (#39)
    * rebase
    
    * 위로가기 버튼 추가

--- src/main/java/com/example/ArtAuction_24/global/base/initData/DevInitData.java
+++ src/main/java/com/example/ArtAuction_24/global/base/initData/DevInitData.java
@@ -12,6 +12,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.util.UUID;
 
 @Configuration
 @RequiredArgsConstructor
@@ -31,11 +35,18 @@ public class DevInitData implements BeforeInitData {
 
             String password = "test123!";
 
-            String thumbnailImg = "freedcs/" + "cf053209-e7d4-4167-a28e-5366090fed61" + ".jpg";
 
             memberService.join("", "admin", password, "admin@test.com", "admin",
                    "010-1234-1234", "대전광역시 서구 대덕대로 179 굿모닝어학원빌딩 9층", "/image/고라파덕.jpg");
 
+            artistService.create("김작가", "kimArtist", "1950-06-28", "010-1234-5678","Artist@naver.com","naver.com","안녕! 나 김작가");
+            artistService.create("나작가", "naArtist", "1968-12-08", "010-4567-5678","Artist@google.com","google.com","안녕! 나 나작가");
+            artistService.create("박작가", "parkArtist", "1999-04-03", "010-9874-5678","Artist@daum.com","daum.com","안녕! 나 박작가");
+            artistService.create("이작가", "leeArtist", "1985-11-02", "010-0582-5678","Artist@kakao.com","kakao.com","안녕! 나 이작가");
+            artistService.create("최작가", "choiArtist", "1975-01-15", "010-8765-1234", "Artist@outlook.com", "outlook.com", "안녕! 나 최작가");
+            artistService.create("정작가", "jungArtist", "1980-07-19", "010-2345-6789", "Artist@yahoo.com", "yahoo.com", "안녕! 나 정작가");
+            artistService.create("강작가", "kangArtist", "1992-03-25", "010-3456-7890", "Artist@msn.com", "msn.com", "안녕! 나 강작가");
+            artistService.create("한작가", "hanArtist", "2001-09-17", "010-5678-0123", "Artist@hotmail.com", "hotmail.com", "안녕! 나 한작가");
 
         };
     }
