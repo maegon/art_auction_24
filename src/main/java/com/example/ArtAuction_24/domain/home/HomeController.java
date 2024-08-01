2791687731f4ad767a34bfa27b2099c09e706047
kimminseop99 <minseopkim99@gmail.com>
2024-08-01 17:12:15 +0900

    main search에 제품검색기능 추가, main page 경매 작품 변경

--- src/main/java/com/example/ArtAuction_24/domain/home/HomeController.java
+++ src/main/java/com/example/ArtAuction_24/domain/home/HomeController.java
@@ -1,15 +1,28 @@
 package com.example.ArtAuction_24.domain.home;
 
+import com.example.ArtAuction_24.domain.artist.entity.Artist;
+import com.example.ArtAuction_24.domain.product.entity.AuctionProduct;
+import com.example.ArtAuction_24.domain.product.service.AuctionProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
+
+    private final AuctionProductService auctionProductService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        List<AuctionProduct> auctionProductList = auctionProductService.findAllAuctionProductOrderByCreateDateDesc();
+
+        model.addAttribute("auctionProductList", auctionProductList);
+
         return "home/main";
     }
 
