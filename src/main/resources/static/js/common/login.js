a446ee383d3b30ef3ca5cd8184c68cc458852dd1
maegon <dlagorhs99@naver.com>
2024-08-01 15:00:21 +0900

    회원가입 디자인 폼 수정

--- src/main/resources/static/js/common/login.js
+++ src/main/resources/static/js/common/login.js
@@ -6,17 +6,10 @@ document.addEventListener("DOMContentLoaded", function() {
     const autoLoginCheckbox = document.getElementById("autoLogin");
 
     const savedUsername = localStorage.getItem("savedUsername");
-    const autoLoginEnabled = localStorage.getItem("autoLoginEnabled");
 
     if (savedUsername) {
         usernameInput.value = savedUsername;
         rememberMeCheckbox.checked = true;
-    }
-    if (autoLoginEnabled === 'true') {
-        autoLoginCheckbox.checked = true;
-        if (usernameInput.value && passwordInput.value) {
-            handleLogin();
-        }
     }
 
     function updateLoginButtonState() {
@@ -33,39 +26,6 @@ document.addEventListener("DOMContentLoaded", function() {
             localStorage.setItem("savedUsername", usernameInput.value);
         } else {
             localStorage.removeItem("savedUsername");
-        }
-        if (autoLoginCheckbox.checked) {
-            localStorage.setItem("autoLoginEnabled", 'true');
-        } else {
-            localStorage.removeItem("autoLoginEnabled");
-        }
-
-        const loginData = {
-            username: usernameInput.value,
-            password: passwordInput.value
-        };
-
-        try {
-            const response = await fetch('/login', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(loginData)
-            });
-
-            if (response.ok) {
-                const result = await response.json();
-                console.log("로그인 성공:", result);
-                window.location.href = '/';
-            } else {
-                const error = await response.json();
-                console.error("로그인 실패:", error.message);
-                alert("로그인 실패: " + error.message);
-            }
-        } catch (error) {
-            console.error("로그인 중 오류 발생:", error);
-            alert("로그인 중 오류가 발생했습니다. 나중에 다시 시도해주세요.");
         }
     }
 
