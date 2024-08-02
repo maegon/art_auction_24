1d1ba0061931b7e97c15492d86575359e0d6a963
maegon <dlagorhs99@naver.com>
2024-08-02 16:53:50 +0900

    로그인 ì정상 작동 í확인

--- src/main/resources/static/js/common/login.js
+++ src/main/resources/static/js/common/login.js
@@ -1,18 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
     const loginButton = document.querySelector(".login-submit");
-    const component-input-id_Input = document.getElementById("component-input-id");
-    const component-input-password_Input = document.getElementById("component-input-password");
+    const loginUsernameInput = document.getElementById("component-input-id");
+    const loginPwInput = document.getElementById("component-input-password");
     const rememberMeCheckbox = document.getElementById("saveId");
 
     const savedUsername = localStorage.getItem("savedUsername");
 
     if (savedUsername) {
-        component-input-id_Input.value = savedUsername;
+        loginUsernameInput.value = savedUsername;
         rememberMeCheckbox.checked = true;
     }
 
     function updateLoginButtonState() {
-        if (component-input-id_Input.value.trim() !== "" && component-input-password_Input.value.trim() !== "") {
+        if (loginUsernameInput.value.trim() !== "" && loginPwInput.value.trim() !== "") {
             loginButton.removeAttribute("disabled");
         } else {
             loginButton.setAttribute("disabled", "true");
@@ -20,15 +20,14 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     function handleLogin(event) {
-        event.preventDefault();
         if (rememberMeCheckbox.checked) {
-            localStorage.setItem("savedUsername", component-input-id_Input.value);
+            localStorage.setItem("savedUsername", loginUsernameInput.value);
         } else {
             localStorage.removeItem("savedUsername");
         }
     }
-    component-input-id_Input.addEventListener("input", updateLoginButtonState);
-    component-input-password_Input.addEventListener("input", updateLoginButtonState);
+    loginUsernameInput.addEventListener("input", updateLoginButtonState);
+    loginPwInput.addEventListener("input", updateLoginButtonState);
     loginButton.addEventListener("click", handleLogin);
 
     updateLoginButtonState();
