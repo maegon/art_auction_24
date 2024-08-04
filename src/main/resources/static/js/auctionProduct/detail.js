09d38da8a0a247012ddb705b884dd397eb247fe6
kimminseop99 <minseopkim99@gmail.com>
2024-08-05 03:26:32 +0900

    돋작품 상세에 돋보기 기능 추가

---
+++ src/main/resources/static/js/auctionProduct/detail.js
@@ -0,0 +1,12 @@
+  $(document).ready(function(){
+            $('.auctionProductTarget').okzoom({
+                width: 150,
+                height: 150,
+                scaleWidth: 400,
+                round: true,
+                background: "#fff",
+                // backgroundRepeat: "repeat",
+                shadow: "0 0 5px #000",
+                border: "1px solid black"
+                });
+        })
