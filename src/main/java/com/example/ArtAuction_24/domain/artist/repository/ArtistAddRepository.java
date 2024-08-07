345c4c29e5298ec3c3c35a2e536866519dde3ff7
kim-chaeyeon <20212621@gm.hannam.ac.kr>
2024-08-07 16:33:50 +0900

    aa

--- src/main/java/com/example/ArtAuction_24/domain/artist/repository/ArtistAddRepository.java
+++ src/main/java/com/example/ArtAuction_24/domain/artist/repository/ArtistAddRepository.java
@@ -7,6 +7,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ArtistAddRepository extends JpaRepository<ArtistAdd, Integer> {
-
     void deleteByArtist(Artist artist);
 }
