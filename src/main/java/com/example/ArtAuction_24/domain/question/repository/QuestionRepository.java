81b23ebdeed006e7b35812079c9c6b0e854b1b6f
tron0318 <tron0318@naver.com>
2024-08-26 18:47:16 +0900

    문의관리 필터

--- src/main/java/com/example/ArtAuction_24/domain/question/repository/QuestionRepository.java
+++ src/main/java/com/example/ArtAuction_24/domain/question/repository/QuestionRepository.java
@@ -1,9 +1,14 @@
 package com.example.ArtAuction_24.domain.question.repository;
 
 import com.example.ArtAuction_24.domain.question.entity.Question;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface QuestionRepository extends JpaRepository<Question, Long> {
+    Page<Question> findByAnsweredTrue(Pageable pageable);
+    Page<Question> findByAnsweredFalse(Pageable pageable);
 }
+
