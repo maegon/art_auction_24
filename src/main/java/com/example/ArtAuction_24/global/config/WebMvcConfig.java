e0033e682902dcff8b3399e697e91423813ad3e0
maegon <dlagorhs99@naver.com>
2024-09-01 09:39:58 +0900

    webMvcConfig addCorsMappings add

--- src/main/java/com/example/ArtAuction_24/global/config/WebMvcConfig.java
+++ src/main/java/com/example/ArtAuction_24/global/config/WebMvcConfig.java
@@ -2,6 +2,7 @@ package com.example.ArtAuction_24.global.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -17,6 +18,15 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 .addResourceLocations("file:///" + fileDirPath + "/");
 
 
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://www.a-auc.art", "https://a-auc.art") // 클라이언트의 URL (배포 시에는 도메인 설정 필요)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
 
